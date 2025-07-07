f075abbcf144db7bd7f6c35afff40448d7f98c02
SneakyGoose <luna27l@outlook.com>
2025-07-08 00:29:44 +0200

    update

--- Tests/Unity/Scripts/SuperRunner/Configs/ChunksList/ChunkListConfig.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-    [CreateAssetMenu(menuName = "Config/ChunkListConfig")]
-    public class ChunkListConfig : ScriptableObject
-    {
-        public List<GameObject> chunkList = new List<GameObject>();
-    }
--- Tests/Unity/Scripts/SuperRunner/Configs/DistanceRecord/DistanceRecordConfig.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Config/DistanceRecordConfig")]
-public class DistanceRecordConfig : ScriptableObject
-{
-    public float newRecord;
-}
